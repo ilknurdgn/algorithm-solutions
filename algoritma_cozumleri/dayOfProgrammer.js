b54888f5e4e48786d15f866e96178e2fe478ef91
ilknur doğan <ilknurdogan254@gmail.com>
2023-05-16 13:00:03 +0300

    dayOfProgrammer change

--- algoritma_cozumleri/dayOfProgrammer.js
+++ algoritma_cozumleri/dayOfProgrammer.js
@@ -1,14 +1,12 @@
 function dayOfProgrammer(year) {
-  if (year > 1918) {
-    if (year % 400 == 0) {
-      return `12.09.${year}`;
-    } else if (year % 4 == 0 && !(year % 100 == 0)) {
+  if (year < 1918) {
+    if (year % 4 == 0) {
       return `12.09.${year}`;
     } else {
       return `13.09.${year}`;
     }
-  } else if (year < 1918) {
-    if (year % 4 == 0) {
+  } else if (year > 1918) {
+    if ((year % 4 == 0 && !(year % 100 == 0)) || year % 400 == 0) {
       return `12.09.${year}`;
     } else {
       return `13.09.${year}`;
