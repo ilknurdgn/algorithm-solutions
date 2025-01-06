9b8f5569a28ccf8b50e236d7dc99b62fa614e2e0
İlknur Doğan <ilknurdogan254@gmail.com>
2025-01-07 00:00:02 +0300

    feat: add Climbing Leaderboard

---
+++ solutions/java/climbingLeaderboard.java
@@ -0,0 +1,26 @@
+public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
+    List<Integer> playerRanks = new ArrayList<>();
+    
+     List<Integer> uniqList = ranked.stream()
+                                .distinct()
+                                .collect(Collectors.toList());
+    
+    // Works correctly but causes timeout
+    // for(int rank : ranked ){
+    //     if(!uniqList.contains(rank)){
+    //         uniqList.add(rank);
+    //     }
+    // } 
+    
+    int point = uniqList.size() - 1;
+    
+    for(int score : player){
+        while(point >= 0 && score >= uniqList.get(point)){
+            point--;
+        }
+        
+        playerRanks.add(point+2);
+    }
+   
+   return playerRanks;
+}
